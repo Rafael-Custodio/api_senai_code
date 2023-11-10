a130245370ffbd619677941a0d8079f8a379d3b9
Rafael-Custodio <rafaelcustodio@msn.com>
2023-11-10 19:56:11 -0300

    update api vsconnect

--- apivsconnect-main/src/main/java/com/senai/apivsconnect/config/SecurityConfig.java
+++ apivsconnect-main/src/main/java/com/senai/apivsconnect/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.senai.apivsconnect.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter securityFilter;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -25,6 +31,7 @@ public class SecurityConfig {
                         // .requestMatchers(HttpMethod.POST, "/servicos").hasRole("CLIENTE")
                         .anyRequest().permitAll()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
